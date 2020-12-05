724879ecdcd31f48b138a614bcfca48ebd34153a
Miguel Korn <miguel.korn@hva.nl>
2020-12-05 08:57:58 +0100

    day5 cleanup

--- day_5/part_1.py
+++ day_5/part_1.py
@@ -3,13 +3,6 @@ input_list = input.read().split('\n')
 print(input_list)
 
 highest = 0
-for p in input_list:
-    p = p.replace('B', '1')
-    p = p.replace('R', '1')
-    p = p.replace('F', '0')
-    p = p.replace('L', '0')
-    total = int(p[:-3], 2) * 8 + int(p[-3:], 2)
-    print(int(p[:-3], 2), int(p[-3:], 2), total)
-    if total > highest:
-        highest = total
-print(highest)
+seats = [int(p.replace('B', '1').replace('R', '1').replace('F', '0').replace('L', '0'), 2) for p in input_list]
+seats.sort()
+print(seats[-1])
--- day_5/part_2.py
+++ day_5/part_2.py
@@ -1,23 +1,11 @@
 input = open('input.txt', 'r')
 input_list = input.read().split('\n')
-print(input_list)
 
 highest = 0
-seats = []
-for p in input_list:
-    p = p.replace('B', '1')
-    p = p.replace('R', '1')
-    p = p.replace('F', '0')
-    p = p.replace('L', '0')
-    total = int(p[:-3], 2) * 8 + int(p[-3:], 2)
-    print(int(p[:-3], 2), int(p[-3:], 2), total)
-    seats.append(total)
-    if total > highest:
-        highest = total
-print(highest)
+seats = [int(p.replace('B', '1').replace('R', '1').replace('F', '0').replace('L', '0'), 2) for p in input_list]
 seats.sort()
-
+print(seats[-1])
 
 for x in range(seats[0], seats[-1]):
-    if x not in seats and x+1 in seats and x-1 in seats:
+    if x not in seats and x + 1 in seats and x - 1 in seats:
         print(x)
