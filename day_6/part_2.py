6d498e33da5f1e2073df7617de87d2fc8220bf4d
Miguel Korn <miguel.korn@hva.nl>
2020-12-06 08:07:48 +0100

    day6

---
+++ day_6/part_2.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+input = open('input.txt', 'r')
+input_list = input.read().split('\n\n')
+
+t = np.sum([len(set.intersection(*[set(x) for x in l.split()])) for l in input_list])
+print(t)
